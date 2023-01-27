4ef6136cb1f79b65cfbe694e7616c50d7334cd06
mostafa hpater <mostafahpater@gmail.com>
2023-01-28 01:07:57 +0200

    add dark mode

--- screens/Home.js
+++ screens/Home.js
@@ -25,11 +25,18 @@ function Home() {
       .get("https://dummyjson.com/products")
       .then((res) => setData(res.data.products));
   }, []);
-  console.log("data")
-  const [theme,setTheme]=useState('light')
+  console.log("data");
+
+  const [theme, setTheme] = useState("light");
   const ExploreData = ({ item, index }) => {
     return (
-      <View key={index} style={[theme==='light'?styles.itemExpLight:styles.itemExpDark, { marginHorizontal:10 }]}>
+      <View
+        key={index}
+        style={[
+          theme === "light" ? styles.itemExpLight : styles.itemExpDark,
+          { marginHorizontal: 10 },
+        ]}
+      >
         <View
           style={{
             justifyContent: "center",
@@ -41,18 +48,25 @@ function Home() {
 
           <MaterialCommunityIcons
             name="heart-circle"
-            style={[theme === "light" ? styles.heartExpLight : styles.heartExpDark]}
+            style={[
+              theme === "light" ? styles.heartExpLight : styles.heartExpDark,
+            ]}
             size={35}
           />
         </View>
         <Text
-          style={[theme==='light'?styles.brandTxtLight:styles.brandTxtDark]}
+          style={[
+            theme === "light" ? styles.brandTxtLight : styles.brandTxtDark,
+          ]}
         >
           {item?.brand}
         </Text>
         <Text
-           style={[theme==='light'?styles.descriptionTxtLight:styles.descriptionTxtDark]}
-         
+          style={[
+            theme === "light"
+              ? styles.descriptionTxtLight
+              : styles.descriptionTxtDark,
+          ]}
           numberOfLines={1}
         >
           {item?.description}
@@ -67,13 +81,22 @@ function Home() {
           }}
         >
           <Text
-             style={[theme==='light'?styles.priceTxtLight:styles.priceTxtDark]}
+            style={[
+              theme === "light" ? styles.priceTxtLight : styles.priceTxtDark,
+            ]}
           >
             ${item?.price}
           </Text>
-          <MaterialIcons name="add-circle"
-           style={[theme==='light'?null:{backgroundColor:'#FFF',borderRadius:50}]}
-          size={33} color="black" />
+          <MaterialIcons
+            name="add-circle"
+            style={[
+              theme === "light"
+                ? null
+                : { backgroundColor: "#FFF", borderRadius: 50 },
+            ]}
+            size={33}
+            color="black"
+          />
         </View>
       </View>
     );
@@ -81,9 +104,9 @@ function Home() {
   const BestSellingData = ({ item, index }) => {
     return (
       <View
-      key={index}
+        key={index}
         style={[
-          theme==='light'?styles.itemExpLight:styles.itemExpDark,
+          theme === "light" ? styles.itemExpLight : styles.itemExpDark,
           {
             flexDirection: "row-reverse",
             width: Dimensions.get("window").width - 40,
@@ -101,12 +124,18 @@ function Home() {
         </View>
         <View style={{ flex: 3 }}>
           <Text
-           style={[theme==='light'?styles.brandTxtLight:styles.brandTxtDark]}
+            style={[
+              theme === "light" ? styles.brandTxtLight : styles.brandTxtDark,
+            ]}
           >
             {item?.brand}
           </Text>
           <Text
-                style={[theme==='light'?styles.descriptionTxtLight:styles.descriptionTxtDark]}
+            style={[
+              theme === "light"
+                ? styles.descriptionTxtLight
+                : styles.descriptionTxtDark,
+            ]}
             numberOfLines={1}
           >
             {item?.description}
@@ -121,12 +150,18 @@ function Home() {
             }}
           >
             <Text
-             style={[theme==='light'?styles.priceTxtLight:styles.priceTxtDark]}
+              style={[
+                theme === "light" ? styles.priceTxtLight : styles.priceTxtDark,
+              ]}
             >
               ${item?.price}
             </Text>
             <MaterialCommunityIcons
-               style={[theme==='light'?styles.arrowsellingLight:styles.arrowsellingDark]}
+              style={[
+                theme === "light"
+                  ? styles.arrowsellingLight
+                  : styles.arrowsellingDark,
+              ]}
               name="arrow-right"
               size={25}
               color="#FFF"
@@ -137,57 +172,88 @@ function Home() {
     );
   };
   return (
-    <View style={[theme==='light'?styles.containerLight:styles.containerDark]}>
+    <View
+      style={[theme === "light" ? styles.containerLight : styles.containerDark]}
+    >
       <StatusBar animated={true} />
       <View style={styles.nav}>
         <Ionicons
           name="user-alt"
-          style={[theme==='light'?styles.userIconLight:styles.userIconDark]}
+          style={[
+            theme === "light" ? styles.userIconLight : styles.userIconDark,
+          ]}
           size={16}
         />
-    {theme==='light'?<Ionicons name="toggle-on"
-    onPress={()=>setTheme('dark')}
-    style={{paddingTop:5}}
-    size={30} color="black" />:
-    <Ionicons name="toggle-off"
-    onPress={()=>setTheme('light')}
-    style={{backgroundColor:'#222233',borderRadius:10,paddingTop:5}} size={30} color="#FFF" />}
+        {theme === "light" ? (
           <Ionicons
+            name="toggle-on"
+            onPress={() => setTheme("dark")}
+            style={{ paddingTop: 5 }}
+            size={30}
+            color="black"
+          />
+        ) : (
+          <Ionicons
+            name="toggle-off"
+            onPress={() => setTheme("light")}
+            style={{
+              backgroundColor: "#222233",
+              borderRadius: 10,
+              paddingTop: 5,
+            }}
+            size={30}
+            color="#FFF"
+          />
+        )}
+        <Ionicons
           name="bars"
-          style={[theme==='light'?styles.barsIconLight:styles.barsIconDark]}
+          style={[
+            theme === "light" ? styles.barsIconLight : styles.barsIconDark,
+          ]}
           size={25}
         />
       </View>
       <View style={styles.searchRow}>
         <Ionicons
           name="shopping-cart"
-          style={[theme==='light'?styles.cartIconLight:styles.cartIconDark]}
+          style={[
+            theme === "light" ? styles.cartIconLight : styles.cartIconDark,
+          ]}
           size={25}
           color="#000"
         />
         <Searchbar
           placeholder="Search"
-          iconColor={theme==='light'?'black':'#b2b2b2'} 
-         
-          style={[theme==='dark'?styles.searchBarDark:styles.searchBarLight]}
+          iconColor={theme === "light" ? "black" : "#b2b2b2"}
+          style={[
+            theme === "dark" ? styles.searchBarDark : styles.searchBarLight,
+          ]}
           placeholderTextColor="#b2b2b2"
         />
       </View>
       <View style={{ marginTop: 20 }}>
-        <Text style={theme==='light'?styles.itemHeadLight:styles.itemHeadDark}>Explore</Text>
-       
-          <FlatList data={data} renderItem={ExploreData} horizontal={true} />
-       
+        <Text
+          style={theme === "light" ? styles.itemHeadLight : styles.itemHeadDark}
+        >
+          Explore
+        </Text>
+
+        <FlatList data={data} renderItem={ExploreData} horizontal={true} />
 
         <View>
-          <Text style={theme==='light'?styles.itemHeadLight:styles.itemHeadDark}>Best Selling</Text>
-          
-            <FlatList
-              data={data.slice(1).slice(-5)}
-              renderItem={BestSellingData}
-              horizontal={true}
-            />
-      
+          <Text
+            style={
+              theme === "light" ? styles.itemHeadLight : styles.itemHeadDark
+            }
+          >
+            Best Selling
+          </Text>
+
+          <FlatList
+            data={data.slice(1).slice(-5)}
+            renderItem={BestSellingData}
+            horizontal={true}
+          />
         </View>
       </View>
     </View>
@@ -196,40 +262,40 @@ function Home() {
 const styles = StyleSheet.create({
   /////// Light Mode
   containerLight: {
-    backgroundColor: '#F5F6FA',
+    backgroundColor: "#F5F6FA",
     padding: 15,
-    height:Dimensions.get('window').height
+    height: Dimensions.get("window").height,
   },
   nav: {
     marginBottom: 20,
     flexDirection: "row",
     justifyContent: "space-between",
   },
-  userIconLight:{
-    color:'#FFF',
+  userIconLight: {
+    color: "#FFF",
     backgroundColor: "#000",
     padding: 13,
-    borderRadius: 7 
+    borderRadius: 7,
   },
-  barsIconLight:{
-    color:'#000',
-    paddingTop: 8 
+  barsIconLight: {
+    color: "#000",
+    paddingTop: 8,
   },
   searchRow: {
     flexDirection: "row",
     justifyContent: "space-between",
   },
-  cartIconLight:{
-    color:'#000',
+  cartIconLight: {
+    color: "#000",
     paddingLeft: 10,
-    paddingTop: 10 
+    paddingTop: 10,
   },
-  searchBarLight:{ 
+  searchBarLight: {
     width: "85%",
-    backgroundColor: "#fff"
+    backgroundColor: "#fff",
   },
   itemHeadLight: {
-    color:'#000',
+    color: "#000",
     fontSize: 25,
     fontWeight: "700",
     letterSpacing: 1,
@@ -255,61 +321,61 @@ const styles = StyleSheet.create({
     borderRadius: 100,
     color: "#f63c41",
   },
-  brandTxtLight:{
+  brandTxtLight: {
     fontSize: 18,
-    color:'#000',
+    color: "#000",
     fontWeight: "800",
     marginTop: 10,
     marginRight: 20,
   },
-  descriptionTxtLight:{
+  descriptionTxtLight: {
     fontSize: 15,
     fontWeight: "500",
     color: "#b2b2b2",
     width: 200,
     marginRight: 20,
   },
-  priceTxtLight:{
-    fontSize:18,
-    color:'#000'
+  priceTxtLight: {
+    fontSize: 18,
+    color: "#000",
   },
-  arrowsellingLight:{
-    backgroundColor: "#000", 
+  arrowsellingLight: {
+    backgroundColor: "#000",
     padding: 4,
-    borderRadius: 6
+    borderRadius: 6,
   },
   //////// Dark Mode
-  containerDark:{
-    backgroundColor: '#201A2D',
+  containerDark: {
+    backgroundColor: "#201A2D",
     padding: 15,
-    height:Dimensions.get('window').height
+    height: Dimensions.get("window").height,
   },
-  userIconDark:{
-    color:'#d6d6d6',
+  userIconDark: {
+    color: "#d6d6d6",
     backgroundColor: "#222233",
     padding: 13,
-    borderRadius: 7 
+    borderRadius: 7,
   },
-  barsIconDark:{
-    color:'#d6d6d6',
-     paddingTop: 8
-     },
-  cartIconDark:{
-     color:'#d6d6d6',
-  paddingLeft: 10,
-   paddingTop: 10 
+  barsIconDark: {
+    color: "#d6d6d6",
+    paddingTop: 8,
   },
-   searchBarDark:{
-     width: "85%",
-    backgroundColor: "#000" ,
+  cartIconDark: {
+    color: "#d6d6d6",
+    paddingLeft: 10,
+    paddingTop: 10,
   },
-  itemHeadDark:{
-    color:'#d6d6d6',
+  searchBarDark: {
+    width: "85%",
+    backgroundColor: "#000",
+  },
+  itemHeadDark: {
+    color: "#d6d6d6",
     fontSize: 25,
     fontWeight: "700",
     letterSpacing: 1,
   },
-  itemExpDark:{
+  itemExpDark: {
     backgroundColor: "#222233",
     padding: 20,
     marginVertical: 20,
@@ -324,28 +390,28 @@ const styles = StyleSheet.create({
     borderRadius: 100,
     color: "#fff",
   },
-  brandTxtDark:{
+  brandTxtDark: {
     fontSize: 18,
-    color:'#FFF',
+    color: "#FFF",
     fontWeight: "800",
     marginTop: 10,
     marginRight: 20,
   },
-  descriptionTxtDark:{
+  descriptionTxtDark: {
     fontSize: 15,
     fontWeight: "500",
     color: "#d6d6d6",
     width: 200,
     marginRight: 20,
   },
-  priceTxtDark:{
-    fontSize:18,
-    color:'#d6d6d6'
+  priceTxtDark: {
+    fontSize: 18,
+    color: "#d6d6d6",
   },
-  arrowsellingDark:{
-  backgroundColor: "#222233", 
-  padding: 4,
-   borderRadius: 6
+  arrowsellingDark: {
+    backgroundColor: "#222233",
+    padding: 4,
+    borderRadius: 6,
   },
 });
 export default Home;
